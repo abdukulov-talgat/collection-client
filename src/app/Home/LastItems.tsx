93cd1deba4483c56d2ef72a1ba1ce6b85c4662af
Abdukulov Talgat <abdukulov.talgat@gmail.com>
2022-10-31 07:00:22 +0500

    Last items order bug fixed.

--- src/app/Home/LastItems.tsx
+++ src/app/Home/LastItems.tsx
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { Grid, List, ListItem, Typography } from '@mui/material';
+import { Grid, Link, List, ListItem, Typography } from '@mui/material';
 import ListItemGroup from '../../common/ListItemGroup/ListItemGroup';
 import { http } from '../../shared/http/http';
 import { apiRoutes } from '../../shared/constants/apiRoutes';
 import { Item } from '../../types/Item';
 import { FormattedMessage, useIntl } from 'react-intl';
+import { Link as RouterLink } from 'react-router-dom';
+import { appRoutes } from '../../shared/constants/appRoutes';
 
 const LastItems = () => {
     const [lastItems, setLastItems] = useState<Item[]>([]);
     const intl = useIntl();
 
     useEffect(() => {
-        http.get(`${apiRoutes.ITEMS}?order=createdAt`).then((response) => setLastItems(response.data.slice(0, 5)));
+        http.get(`${apiRoutes.ITEMS}?order=createdAt&direction=DESC`).then((response) =>
+            setLastItems(response.data.slice(0, 5))
+        );
     }, []);
 
     return (
@@ -24,7 +28,9 @@ const LastItems = () => {
                     <ListItem key={item.id}>
                         <Grid container rowGap={1}>
                             <ListItemGroup xs={6} sm={6} title={intl.formatMessage({ id: 'app.lastItems.itemName' })}>
-                                {item.name}
+                                <Link component={RouterLink} to={`${appRoutes.ITEM_ROOT}/${item.id}`}>
+                                    {item.name}
+                                </Link>
                             </ListItemGroup>
                             <ListItemGroup
                                 justifyContent="flex-end"
@@ -32,7 +38,9 @@ const LastItems = () => {
                                 sm={6}
                                 title={intl.formatMessage({ id: 'app.lastItems.collectionName' })}
                             >
-                                {item.collectionName}
+                                <Link component={RouterLink} to={`${appRoutes.COLLECTION_ROOT}/${item.collectionId}`}>
+                                    {item.collectionName}
+                                </Link>
                             </ListItemGroup>
                             <ListItemGroup
                                 justifyContent="flex-end"
